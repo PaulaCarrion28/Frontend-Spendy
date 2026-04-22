86e4e866e11fe31925108994459208d31dfd8750
Sofia Escobar <sofiasofiaescobar1@gmail.com>
2026-04-22 11:52:02 -0500

    feat: Login + verificación de usuario LocalStorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,42 @@
-// Login.jsx
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
-import "./Forms";
+import "./Forms.css";
+import { validarLogin } from "../helpers/local-storage";
 
 const Login = () => {
-  return (
-    <>
-      <div className="contenedor">
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
+  const recargaLogin = (e) => {
+    e.preventDefault();
+    const usuarioAutenticado = validarLogin(email, password);
+
+    if (usuarioAutenticado) {
+      localStorage.setItem('sesion_activa', JSON.stringify(usuarioAutenticado));
+      navigate('/');
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  };
+
+  return (
+    /* Clase main-screen: Controla que el Footer siempre esté al fondo */
+    <div className="main-screen">
+      <div className="contenedor">
         <div className="formulario">
           <h2>Iniciar Sesión</h2>
 
-
-          <form>
-
+          <form onSubmit={recargaLogin}>
             <div className="input-group">
-              <label>Correo</label>
+              <label>Correo Electrónico</label>
               <input
                 type="email"
-                placeholder="Ingresa tu correo"
+                placeholder="correo@ejemplo.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
@@ -27,29 +44,24 @@ const Login = () => {
               <label>Contraseña</label>
               <input
                 type="password"
-                placeholder="Ingresa tu contraseña"
+                placeholder="Tu contraseña"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
-            <button type="submit">
-              Ingresar
-            </button>
-
+            <button type="submit">Ingresar</button>
           </form>
 
           <div className="newaccount">
-            ¿No tienes cuenta?{" "}
-            <Link to="/register">
-              Regístrate
-            </Link>
+            ¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link>
           </div>
-
         </div>
-
       </div>
-
+      
       <Footer />
-    </>
+    </div>
   );
 };
 
